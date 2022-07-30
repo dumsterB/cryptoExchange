5b9c64c7ff0b58b98ac6dc0d4e5f358b537db6bb
Igor <igorgordilov22@gmail.com>
2022-07-30 19:51:27 +0300

    app assets

--- src/main.js
+++ src/main.js
@@ -1,37 +1,14 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-// import VButton from './components/common/VButton.vue'
-// import VIcon from './components/common/VIcon.vue'
-import router from './router'
-import locales from './locales'
-// import { loadChains } from './services/api/repositories/chains'
-// import { useChainsStore } from './stores/chains'
-
-
-
-// app.component('VButton', VButton);
-// app.component('VIcon', VIcon);
+import App from './App.vue';
+import router from './app/router';
+import locales from './app/locales';
 
 const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
-app.use(locales)
-
-// const chainsStore = useChainsStore();
-
-// chainsStore.chains = loadChains();
+app.use(createPinia());
+app.use(router);
+app.use(locales);
 
 app.mount('#app');
-
-
-// prod version
-// const app = createApp(App)
-
-// app.use(createPinia())
-// app.use(router)
-// app.use(translation)
-
-// app.mount('#app')
