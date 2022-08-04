2ca96425186bb107dd56904e876eb7f75e447c0f
Igor <igorgordilov22@gmail.com>
2022-08-04 21:59:03 +0300

    layouts

---
+++ src/components/Layouts/Default/DefaultLayout.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: 'DefaultLayout'
+};
+</script>
+
+<script setup>
+import { VContainer } from '@/uikit';
+import { TheHeader, TheSidebar } from '@/components/Menus';
+import { VerificationAlert } from '@/components/User';
+import { ErrorBoundary } from '@/components/Error';
+</script>
+
+<template>
+    <div :class="styles.wrapper">
+        <div :class="styles.container">
+            
+            <TheHeader />
+            
+            <TheSidebar />
+
+            <ErrorBoundary>
+                <VContainer>
+                    <VerificationAlert />
+
+                    <slot />
+                </VContainer>
+            </ErrorBoundary>
+            
+        </div>
+    </div>
+</template>
+
+<style lang="scss" module="styles">
+@import './DefaultLayout.module.scss';
+</style>
---
+++ src/components/Layouts/index.js
@@ -0,0 +1,13 @@
+import { defineAsyncComponent } from 'vue';
+
+export const defaultLayout = 'DefaultLayout';
+// export const assetLayout = 'AssetLayout';
+export const authLayout = 'AuthLayout';
+// export const emptyLayout = 'EmptyLayout';
+
+export const layouts = {
+    [defaultLayout]: defineAsyncComponent(() => import('./Default/DefaultLayout.vue')),
+    [authLayout]: defineAsyncComponent(() => import('./Auth/AuthLayout.vue')),
+    // [assetLayout]: defineAsyncComponent(() => import('./Asset/AssetLayout.vue')),
+    // [emptyLayout]: defineAsyncComponent(() => import('./Empty/EmptyLayout.vue'))
+};
