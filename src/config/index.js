f36519af7f14915c5d67633f9b55280b908140bc
Igor <igorgordilov22@gmail.com>
2022-08-04 22:04:07 +0300

    config file

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+export const BASE_URL = 'https://api.whitex.finance';
+export const SOCKET_URL = 'https://api.whitex.finance';
+
+export const MOCK_API_CALLS = true;
+
+export const ICONS = import.meta.globEager('../app/assets/icons/*.svg');
