f360c5e0dd0edb38656b49f355ad2281266b99f7
Igor <igorgordilov22@gmail.com>
2022-08-07 20:22:40 +0300

    Fix auth otp

--- src/views/Auth/Register/Flow/hooks/useOtpConfirmations.js
+++ src/views/Auth/Register/Flow/hooks/useOtpConfirmations.js
@@ -1,17 +1,24 @@
 import { useOtp } from '@/hooks/useOtp';
+import { getWaitListID, setWaitListID } from '@/states/user/services/register';
 import { sendEmailOtp, submitEmailOtp, sendPhoneOtp, submitPhoneOtp } from '@/states/user/fetch/register';
 
 export function useOtpConfirmations(toNextStep) {
     const emailOtp = useOtp({
-        sendFetch: sendEmailOtp,
-        submitFetch: submitEmailOtp,
-        handleSuccessSubmit: toNextStep
+        sendFetch: () => sendEmailOtp(getWaitListID()),
+        submitFetch: code => submitEmailOtp(getWaitListID(), code),
+        handleSuccessSubmit: ({ waitListID }) => {
+            toNextStep();
+            setWaitListID(waitListID);
+        }
     });
     
     const phoneOtp = useOtp({
-        sendFetch: sendPhoneOtp,
-        submitFetch: submitPhoneOtp,
-        handleSuccessSubmit: toNextStep
+        sendFetch: () => sendPhoneOtp(getWaitListID()),
+        submitFetch: code => submitPhoneOtp(getWaitListID(), code),
+        handleSuccessSubmit: ({ waitListID }) => {
+            toNextStep();
+            setWaitListID(waitListID);
+        }
     });
 
     return {
